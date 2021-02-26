369b53c07ce87bc73e5e8799b47079dfa0c6ac9a
Elena-Weber <crochetmiracles@gmail.com>
2021-02-25 21:15:45 -0500

    commit tree 81d0578a41d6c714a13948514452765cd38fa289

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
+def sort_array_asc(array) #done
+array.sort
+end
 
+def sort_array_desc(array) #done
+array.sort.reverse
+end
+
+def sort_array_char_count(array)
+    array = array.sort_by {|word| word.length}
+end
+
+def swap_elements(array) #done
+array[1], array[2] = array[2], array[1]
+array
+end
+
+def reverse_array(array) #done
+array.reverse
+end
+
+def kesha_maker(array) #done
+array.each do |word|
+    word[2] = "$"
+end
+end
+
+def find_a(array) #done
+array.select {|word| word.start_with?("a")}
+end
+
+def sum_array(array) #done
+array.inject(:+)
+end
+
+def add_s(array) #done
+array.each_with_index.collect do |word, i|
+    if i != 1
+        word + "s"
+    elsif i == 1
+        word
+    end
+end
+end
